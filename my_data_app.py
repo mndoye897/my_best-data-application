316a805aeaccd3a8883fc78f574b2ee9d89dd63e
mndoye897 <mndoye897@gmail.com>
2026-01-08 14:48:04 +0000

    Refactor markdown and loading function in my_data_app.py

--- my_data_app.py
+++ my_data_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.markdown("<h1 style='text-align: center; color: black;'>MY DATA APP</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>MY DATA APP.</h1>", unsafe_allow_html=True)
 
 st.markdown("""
 This app allows you to download scraped data on motocycles from expat-dakar 
@@ -41,5 +41,6 @@ for i in range(1,6):
 
 
  
+
 
 
